da2e3dcb7dab21a65d550cbf04d2b9a0765635fd
HumzaBaig <humzacodemail@gmail.com>
2023-08-09 11:08:33 -0500

    set up create user route and controller method

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,5 +6,23 @@ export const userController = {
     async index(req: Request, res: Response) {
         const users = await prisma.user.findMany();
         return res.json(users);
+    },
+
+    async create(req: Request, res: Response) {
+        const userData = req.body;
+
+        try {
+            const user = await prisma.user.create({
+                data: {
+                    fname: userData.fname,
+                    lname: userData.lname,
+                },
+            });
+
+            return res.json({ user });
+        } catch (error) {
+            console.log(`[USER_CREATION_ERROR] ${error}`);
+            return res.status(500).json({error: 'An error occurred while creating the user.'});
+        }
     }
 }
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,5 +4,6 @@ import { userController } from "../controllers/user.controller";
 const routes = Router();
 
 routes.get("/", userController.index);
+routes.post("/", userController.create);
 
 export default routes;
